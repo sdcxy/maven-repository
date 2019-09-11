783438916a97ca45171ae931f7737307e107831b
lxx <qq2378101302@163.com>
2019-09-11 17:11:38 +0800

    修改默认配置父级包

--- auto-generate-code/src/main/java/com/github/sdcxy/modules/common/entity/DataSource.java
+++ auto-generate-code/src/main/java/com/github/sdcxy/modules/common/entity/DataSource.java
@@ -2,12 +2,7 @@ package com.github.sdcxy.modules.common.entity;
 
 import lombok.Data;
 
-/**
- * @ClassName DataSource
- * @Description TODO
- * @Author lxx
- * @Date 2019/9/10 0:48
- **/
+
 @Data
 public class DataSource {
 
